71160478f1f4726a6979ad84b593fe30ee9225cb
Adarsh Sharma <119839830+AdarshSharma-v10@users.noreply.github.com>
2025-08-05 21:45:42 +0530

    Update build.sh

--- backend/build.sh
+++ backend/build.sh
@@ -1,9 +1,6 @@
 #!/usr/bin/env bash
 set -e
 
-apt-get update
-apt-get install -y build-essential libjpeg-dev zlib1g-dev libfreetype6-dev
-
 pip install --upgrade pip setuptools wheel
 pip install -r requirements.txt
 
